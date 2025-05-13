ab49d7d0d655fbca3da6aca54e6da37a832033b0
tuantu2609 <tuantums10@gmail.com>
2025-05-13 13:09:12 +0700

    Fix metro config inheritance

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,19 @@
+// const { getDefaultConfig } = require("expo/metro-config");
+// const { withNativeWind } = require("nativewind/metro");
+
+// const config = getDefaultConfig(__dirname);
+
+// module.exports = withNativeWind(config, { input: "./app/global.css" });
+// metro.config.js
 const { getDefaultConfig } = require("@expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname);
+// Lấy cấu hình gốc từ Expo
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./app/global.css" });
+// Apply NativeWind sau khi đã có cấu hình chuẩn từ Expo
+const config = withNativeWind(defaultConfig, {
+  input: "./app/global.css",
+});
+
+module.exports = config;
